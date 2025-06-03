5bdb9982fce1c61c25cf81a8b7342aaee63006fd
andrewSmellz <andrewnaquino@gmail.com>
2025-06-03 16:10:01 -0400

    finish item name and lore feeling dead rn

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,34 @@ use std::collections::HashMap;
 fn main() {
     let mut username = String::new();
     let mut enchantments: HashMap<String, String> = HashMap::new();
-
+    let mut enchants_command = String::new();
     println!("enter username");
     io::stdin().read_line(&mut username).unwrap();
+
+    let mut option = String::new();
+    let mut item_name = String::new();
+    println!("do you want to add name to item(y/n)");
+    io::stdin().read_line(&mut option).unwrap();
+    if option.trim() == "y"{
+        println!("enter name:");
+        io::stdin().read_line(&mut item_name).unwrap();
+    }
 
     loop{
         let mut enchantment = String::new();
         let mut strength = String::new();
-        println!("enter name of enchantment");
-        println!("enter 'q' to quit");
+        println!("enter name of enchantment(enter 'q' to quit):");
         io::stdin().read_line(&mut enchantment).unwrap();
         if enchantment.trim() == "q"{
             break;
         }
-        println!("enter strength of enchantment");
+        println!("enter strength of enchantment:");
         io::stdin().read_line(&mut strength).unwrap();
 
         enchantments.insert(enchantment, strength);
     }
 
-    let mut enchants_command = String::new();
+    
     for (e,s) in &enchantments{
         enchants_command.push_str(&format!("'{}':{},",e.trim(),s.trim()));
     }
